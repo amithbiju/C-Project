596465c71987f652248654179334c8db57bb4872
Amith Biju <amithbiju1@gmail.com>
2023-12-20 01:26:01 +0530

    final

--- inventory_test.c
+++ inventory_test.c
@@ -31,6 +31,7 @@ struct Order {
 struct Customer {
     int customerID;
     char name[50];
+    char pass[50];
     struct Order *orders;
     struct Customer *nextCustomer;
 };
@@ -49,12 +50,15 @@ void processOrder(TreeNode* root, int productId, int quantity);
 void freeTree(TreeNode* root);
 TreeNode* changedetail(TreeNode* root,int productId);
 TreeNode* changestock(TreeNode* root,int productId);
-void addCustomer(int id, char cname[]) ;
+void addCustomer(int id, char cname[],char pass[]) ;
 void addOrder(int customerID, int orderID, int productID, int qty);
 int addprice(TreeNode* root, int productId, int qty) ;
 void displayCustomerOrders();
+struct Customer *loginUser(int userID, const char pass[]);
 void readFromFile(TreeNode** root, const char* filename);
 void saveToFile(TreeNode* root, const char* filename);
+void saveDataToFile(const char *filename);
+void loadDataFromFile(const char *filename);
 
 
 
@@ -67,17 +71,21 @@ int main() {
     int choice3;
     int choice4;
     readFromFile(&root, "product.txt");
+    loadDataFromFile("customer.txt");
+    printf("\n--------------------------------------------------------------------------------------------\n");
     printf("welcome to XYZ retail store-your one stop destination for all your needs\n\nAbout us:At XYZ we believe in more than just providing top notch services;\n");
     printf("we are dedicated to creating an unparallel shopping experiences for all our customers\n");
     printf("contact us: 9835378393\nemail id:XYZretails@gmail.com\n");
     printf("location:Austria-Vienna\n\n");
+    printf("\n--------------------------------------------------------------------------------------------\n");
    
     do {
         
         printf("please mention your access control");
-        printf("\n1.admin \n");
-        printf("2. seller\n");
-        printf("3. customer\n");
+        printf("\n--------------------------------------------------------------------------------------------\n");
+        printf("\n1.Admin \n");
+        printf("2. Supplier\n");
+        printf("3. Customer\n");
         printf("4. Save to File\n");
         printf("5. Exit\n");
         printf("Enter your choice: ");
@@ -87,7 +95,7 @@ int main() {
             case 1: {
                 int pass=2226;
                 int val;
-                printf("enter the authentication password\n");
+                printf("Enter the authentication password : \n");
                 scanf("%d",&val);
                 if(val!=pass)
                 {
@@ -98,23 +106,23 @@ int main() {
                 do{
                    
                     printf("\n1.Add Product \n2.Display Products\n3.edit product details\n4.displayCustomerOrders\n5.exit\n");
-                    printf("Enter your choice: ");
+                    printf("Enter your choice : ");
                     scanf("%d", &choice1);
                     switch (choice1){
                     case 1:{
                         Product newProduct;
-                        printf("Enter product name: ");
+                        printf("Enter product name : ");
                         scanf("%s", newProduct.productName);
-                        printf("Enter product Category");
+                        printf("Enter product Category : ");
                         scanf("%s", newProduct.category);
-                        printf("Enter cost price: ");
+                        printf("Enter cost price : ");
                         scanf("%f", &newProduct.costPrice);
-                        printf("Enter selling price: ");
+                        printf("Enter selling price : ");
                         scanf("%f", &newProduct.sellingPrice);
-                        printf("Enter quantity in stock: ");
+                        printf("Enter quantity in stock : ");
                         scanf("%d", &newProduct.quantityInStock);
 
-                        newProduct.productId = rand(); // Generating a random product ID for simplicity
+                        newProduct.productId = rand()+1000; // Generating a random product ID for simplicity
                         root = insert(root, newProduct);
                         printf("Product added successfully!\n");
                         
@@ -126,7 +134,7 @@ int main() {
                     }
                     case 3:
                           {
-                            printf("enter the product id");
+                            printf("enter the product id : ");
                             int id;
                             scanf("%d",&id);
                             root= changedetail(root,id);
@@ -153,13 +161,16 @@ int main() {
                 do{
                    
                     printf("\n1.Add Product \n2.Display Products\n3.Add Quantity\n4.exit\n");
-                    printf("Enter your choice: ");
+                    printf("Enter your choice : ");
                     scanf("%d", &choice2);
                     switch (choice2){
                     case 1:{
                         Product newProduct;
-                        printf("Enter product name: ");
+                        printf("Enter product name : ");
                         scanf("%s", newProduct.productName);
+                        
+                        printf("Enter product Category : ");
+                        scanf("%s", newProduct.category);
                         printf("Enter cost price: ");
                         scanf("%f", &newProduct.costPrice);
                         printf("Enter selling price: ");
@@ -167,7 +178,7 @@ int main() {
                         printf("Enter quantity in stock: ");
                         scanf("%d", &newProduct.quantityInStock);
 
-                        newProduct.productId = rand(); // Generating a random product ID for simplicity
+                        newProduct.productId = rand()+1000; // Generating a random product ID for simplicity
                         root = insert(root, newProduct);
                         printf("Product added successfully!\n");
                         
@@ -179,7 +190,7 @@ int main() {
                     }
                     case 3:
                           {
-                            printf("enter the product id");
+                            printf("enter the product id : ");
                             int id;
                             scanf("%d",&id);
                             root= changestock(root,id);
@@ -209,13 +220,16 @@ int main() {
                     scanf("%d", &choice3);
                     switch (choice3){
                     case 1:{
-                        char name[67];
+                        char name[67], pass[68];
                         int id;
-                        printf("Enter Your name");
+                        printf("Enter Your name : ");
                         scanf("%s",name);
-                        printf("Enter the userID ");
+                        printf("Enter the userID : ");
                         scanf("%d",&id);
-                        addCustomer(id,name);
+                        printf("Enter the Password : ");
+                        scanf("%s",pass);
+                        addCustomer(id,name,pass);
+                        saveDataToFile("customer.txt");
                         int productId, quantity;
                         
                         do{
@@ -224,7 +238,7 @@ int main() {
                             char cate[25];
                             switch (choice4){
                                 case 1:{
-                                    printf("Enter the Category you want  :[]");
+                                    printf("Enter the Category you want  :");
                                     scanf("%s",cate);
                                     displaycate(root,cate);
                                     break;
@@ -256,7 +270,52 @@ int main() {
                         break;
                     }
                     case 2 :{
-                        //displayProducts(root);
+                        char pass[68];
+                        int id;
+                        int productId, quantity;
+                        printf("Enter the USERID :");
+                        scanf("%d",&id);
+                        printf("Enter the Password : ");
+                        scanf("%s",pass);
+                        struct Customer *logedinCustomer = loginUser(id,pass);
+                        if (logedinCustomer != NULL){
+                            do{
+                            printf("1.Display Products \n2.Buy Products \n 3.EXIT\n Enter any option");
+                            scanf("%d", &choice4);
+                            char cate[25];
+                            switch (choice4){
+                                case 1:{
+                                    printf("Enter the Category you want  :");
+                                    scanf("%s",cate);
+                                    displaycate(root,cate);
+                                    break;
+                                }
+                                case 2:{
+                                    int num;
+                                    int sum=0;
+                                    printf("How many products do you want?");
+                                    scanf("%d",&num);
+                                    for(int i=0;i<num;i++){
+                                        printf("Enter product ID for the order: ");
+                                        scanf("%d", &productId);
+                                        printf("Enter quantity for the order: ");
+                                        scanf("%d", &quantity);
+                                        sum +=addprice(root,productId,quantity );
+                                        processOrder(root, productId, quantity);
+                                        addOrder(id, rand(),productId ,quantity );
+                                    }
+                                    printf("\nTotal Amount for the products -> %d",sum);
+                                    printf("\nThank You for shopping with us....❤️");
+                                    break;
+                                }
+                                case 3:{
+                                    printf("Exiting the Section.\n");
+                                    break;
+                                }
+                            }
+                        }while(choice4!= 3);
+                        break;
+                        }
                         break;
                     }
                     case 3:{
@@ -382,11 +441,11 @@ void freeTree(TreeNode* root) {
         return root;
     }
     printf("ProductID: %d, ProductName: %s, CostPrice: %.2f, SellingPrice: %.2f, QuantityInStock: %d\n",
-               root->product.productId, root->product.productName, root->product.costPrice,
-               root->product.sellingPrice, root->product.quantityInStock);
+               productNode->product.productId, productNode->product.productName, productNode->product.costPrice,
+               productNode->product.sellingPrice, productNode->product.quantityInStock);
     printf("Enter the New prize");
     scanf("%d",&newpr);
-    root->product.sellingPrice= newpr;
+    productNode->product.sellingPrice= newpr;
     printf("You have sucessfully changed the prize!!");
     return root;
 }
@@ -399,17 +458,17 @@ TreeNode * changestock(TreeNode* root, int productId) {
         return root;
     }
     printf("ProductID: %d, ProductName: %s, CostPrice: %.2f, SellingPrice: %.2f, QuantityInStock: %d\n",
-               root->product.productId, root->product.productName, root->product.costPrice,
-               root->product.sellingPrice, root->product.quantityInStock);
+               productNode->product.productId, productNode->product.productName, productNode->product.costPrice,
+               productNode->product.sellingPrice, productNode->product.quantityInStock);
     printf("Enter amount of stock you have");
     scanf("%d",&newpr);
-    root->product.quantityInStock += newpr ;
+    productNode->product.quantityInStock += newpr ;
     printf("You have sucessfully changed the Quantity!!");
     return root;
 }
 
 // Function to add a new customer
-void addCustomer(int id, char cname[]) {
+void addCustomer(int id, char cname[],char pass[]) {
     struct Customer *newCustomer = (struct Customer*)malloc(sizeof(struct Customer));
     if (newCustomer == NULL) {
         printf("Memory allocation failed.\n");
@@ -417,6 +476,7 @@ void addCustomer(int id, char cname[]) {
     }
     newCustomer->customerID = id;
     strcpy(newCustomer->name, cname);
+    strcpy(newCustomer->pass, pass);
     newCustomer->orders = NULL;
     newCustomer->nextCustomer = customerList;
     customerList = newCustomer;
@@ -475,6 +535,25 @@ void displayCustomerOrders() {
     }
 }
 
+// Function to log in users by validating user ID and password
+struct Customer *loginUser(int userID, const char pass[]) {
+    struct Customer *currentCustomer = customerList;
+    while (currentCustomer != NULL) {
+        if (currentCustomer->customerID == userID && strcmp(currentCustomer->pass, pass) == 0) {
+            // User ID and password match
+            printf("Login successful!\n");
+            printf("**********************************\n");
+            printf("WELCOME BACK %s \n",currentCustomer->name);
+            printf("**********************************\n");
+            
+            return currentCustomer;
+        }
+        currentCustomer = currentCustomer->nextCustomer;
+    }
+    printf("Invalid user ID or password. Login failed.\n");
+    return NULL;
+}
+
 // Function to read product data from a file and build the binary search tree
 void readFromFile(TreeNode** root, const char* filename) {
     FILE* file = fopen(filename, "r");
@@ -513,4 +592,86 @@ void saveToFile(TreeNode* root, const char* filename) {
     saveToFileHelper(root, file);
 
     fclose(file);
+}
+
+// Function to save customer data to a file
+void saveDataToFile(const char *filename) {
+    FILE *file = fopen(filename, "w");
+    if (file == NULL) {
+        printf("Error opening file for writing.\n");
+        return;
+    }
+
+    struct Customer *currentCustomer = customerList;
+    while (currentCustomer != NULL) {
+        // Write customer details to the file
+        fwrite(currentCustomer, sizeof(struct Customer), 1, file);
+
+        // Write each order for the customer to the file
+        struct Order *currentOrder = currentCustomer->orders;
+        while (currentOrder != NULL) {
+            fwrite(currentOrder, sizeof(struct Order), 1, file);
+            currentOrder = currentOrder->nextOrder;
+        }
+
+        currentCustomer = currentCustomer->nextCustomer;
+    }
+
+    fclose(file);
+    printf("Data saved to file successfully.\n");
+}
+
+// Function to load customer data from a file
+void loadDataFromFile(const char *filename) {
+    FILE *file = fopen(filename, "r");
+    if (file == NULL) {
+        printf("Error opening file for reading.\n");
+        return;
+    }
+
+    // Clear existing customer list
+    while (customerList != NULL) {
+        struct Customer *temp = customerList;
+        customerList = customerList->nextCustomer;
+        free(temp);
+    }
+
+    struct Customer tempCustomer;
+    struct Order tempOrder;
+
+    // Read customer details and orders from the file
+    while (fread(&tempCustomer, sizeof(struct Customer), 1, file) > 0) {
+        // Add the customer to the linked list
+        struct Customer *newCustomer = (struct Customer*)malloc(sizeof(struct Customer));
+        if (newCustomer == NULL) {
+            printf("Memory allocation failed.\n");
+            fclose(file);
+            return;
+        }
+        memcpy(newCustomer, &tempCustomer, sizeof(struct Customer));
+        newCustomer->orders = NULL;
+        newCustomer->nextCustomer = customerList;
+        customerList = newCustomer;
+
+        // Read and add orders for the customer
+        while (fread(&tempOrder, sizeof(struct Order), 1, file) > 0) {
+            if (tempOrder.orderID == tempCustomer.customerID) {
+                struct Order *newOrder = (struct Order*)malloc(sizeof(struct Order));
+                if (newOrder == NULL) {
+                    printf("Memory allocation failed.\n");
+                    fclose(file);
+                    return;
+                }
+                memcpy(newOrder, &tempOrder, sizeof(struct Order));
+                newOrder->nextOrder = newCustomer->orders;
+                newCustomer->orders = newOrder;
+            } else {
+                // Order does not belong to the current customer, break the inner loop
+                break;
+            }
+        }
+    }
+
+    fclose(file);
+    printf("Data loaded from file successfully.\n");
 }
